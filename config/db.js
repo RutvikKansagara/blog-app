b9ed236455925ee5bc1817f8045f1a24a025a680
RutvikKansagara <rutvikkansagara7852@gmail.com>
2023-11-05 10:49:40 +0530

    backend of blog

--- config/db.js
+++ config/db.js
@@ -1,16 +1,37 @@
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv").config();
+
+// const connectToMongoDb = () => {
+//     try {
+//         const MONGO_URI = process.env.MONGO_URI;
+
+//         mongoose.connect(MONGO_URI);
+//         console.log("connected to mongodb");
+//     } catch (error) {
+//         console.log("error connecting to mongodb: " + error);
+//         process.exit();
+//     }
+// }
+
+// module.exports = connectToMongoDb;
+
 const mongoose = require("mongoose");
+
 const dotenv = require("dotenv").config();
 
-const connectToMongoDb = () => {
+const connectToMongoDb = async () => {
     try {
         const MONGO_URI = process.env.MONGO_URI;
 
-        mongoose.connect(MONGO_URI);
-        console.log("connected to mongodb");
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+        });
+
+        console.log("Successfully connected to the database");
     } catch (error) {
-        console.log("error connecting to mongodb: " + error);
-        process.exit();
+        console.error("Error connecting to the database:", error.message);
+        process.exit(1); // Exit with a non-zero status code to indicate failure
     }
-}
+};
 
 module.exports = connectToMongoDb;
